6a7e31675ac8847908c5194fd92d2a60234edd5b
Mino41 <124701231+MinoRabarison@users.noreply.github.com>
2024-01-27 14:12:20 +0300

    Update User.java

--- parcelle Spring/demo/src/main/java/com/example/demo/domaine/User.java
+++ parcelle Spring/demo/src/main/java/com/example/demo/domaine/User.java
@@ -1,63 +1,63 @@
-package com.example.demo.domaine;
-import com.example.demo.domaine.Terrain;
+// package com.example.demo.domaine;
+// import com.example.demo.domaine.Terrain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
 
-@Entity
-public class User 
-{
-    @GeneratedValue
+// @Entity
+// public class User 
+// {
+//     @GeneratedValue
 
-    int idUser;
-    String pseudo;
-    String password;
-    String email;
-    Terrain[] terrain;
+//     int idUser;
+//     String pseudo;
+//     String password;
+//     String email;
+//     Terrain[] terrain;
 
-    public User(){}
+//     public User(){}
 
-    public int getIdUser() {
-        return idUser;
-    }
+//     public int getIdUser() {
+//         return idUser;
+//     }
 
-    public void setIdUser(int idUser) {
-        this.idUser = idUser;
-    }
+//     public void setIdUser(int idUser) {
+//         this.idUser = idUser;
+//     }
 
-    public String getPseudo() {
-        return pseudo;
-    }
+//     public String getPseudo() {
+//         return pseudo;
+//     }
 
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-    }
+//     public void setPseudo(String pseudo) {
+//         this.pseudo = pseudo;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public Terrain[] getTerrain() {
-        return terrain;
-    }
+//     public Terrain[] getTerrain() {
+//         return terrain;
+//     }
 
-    public void setTerrain(Terrain[] terrain) {
-        this.terrain = terrain;
-    }
+//     public void setTerrain(Terrain[] terrain) {
+//         this.terrain = terrain;
+//     }
 
-}
+// }
